c3e70d4fb113eb48592e6d79945961e35a851b5f
Big-Zude <zudemwango@gmail.com>
2021-04-14 12:32:46 +0200

    added react-ga

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 
-ReactGA.initialize('UA-153096172-1'); // add your tracking id here.
-ReactGA.pageview(window.location.pathname + window.location.search);
+const history = createBrowserHistory();
+ReactGA.initialize('UA-153096172-1');
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+// ReactGA.initialize('UA-153096172-1'); // add your tracking id here.
+// ReactGA.pageview(window.location.pathname + window.location.search);
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
+ReactDOM.render(
+  <Router history={history}>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

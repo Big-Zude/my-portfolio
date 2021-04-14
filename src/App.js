5ca3e25b7a449961da57eb5983962ece4c4aec32
Big-Zude <zudemwango@gmail.com>
2021-04-14 12:50:37 +0200

    added react ga in useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Home from "./components";
-import Resume from "./components/Resume";
-import Navbar from "./components/Navbar";
-import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
-import Article from './components/Articles'
+import React, { useEffect } from 'react';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Home from './components';
+import Resume from './components/Resume';
+import Navbar from './components/Navbar';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Article from './components/Articles';
+import ReactGA from 'react-ga';
 
-import "./App.css";
+import './App.css';
+ReactGA.initialize('UA-153096172-1');
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.set({ page: location.pathname }); // Update the user's current page
+    ReactGA.pageview(location.pathname); // Record a pageview for the given page
+  }, [location.pathname]);
+
   return (
     <React.Fragment>
       <CssBaseline />
--- src/index.js
+++ src/index.js
@@ -3,20 +3,14 @@ import ReactDOM from "react-dom";
 import {BrowserRouter as Router} from 'react-router-dom'
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
 
-const history = createBrowserHistory();
-ReactGA.initialize('UA-153096172-1');
-history.listen((location) => {
-  ReactGA.set({ page: location.pathname }); // Update the user's current page
-  ReactGA.pageview(window.location.pathname); // Record a pageview for the given page
-});
+
+
 // ReactGA.initialize('UA-153096172-1'); // add your tracking id here.
 // ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
-  <Router history={history}>
+  <Router>
     <App />
   </Router>,
   document.getElementById('root')
